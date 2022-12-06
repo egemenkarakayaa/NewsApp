9a0a22242549cd1b904f1104448d5216bfe95201
egemenkarakayaa <egemenkarakayaa@gmail.com>
2022-12-06 11:38:57 +0300

    Added tableview to the project.

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -5,14 +5,26 @@
 //  Created by Egemen Karakaya on 2.12.2022.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+       return table
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "News"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .white
         
         APICaller.shared.getTopStories { result in
@@ -27,5 +39,29 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    // TableView Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath)
+        cell.textLabel?.text = "denemekfsg"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
     
 }
